b02b902d77e2f0c37d3bcc7d1f4a7dba58a94b83
Terry Lamb <terrylamb@gmail.com>
2023-08-28 17:33:49 -0400

    add validation model testing for plane.rb

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,3 +1,8 @@
 class Plane < ApplicationRecord
-  
+  belongs_to :airport
+
+  validates_presence_of :flight_name
+  validates_presence_of :plane_model
+  validates_presence_of :capacity
+  validates :on_time, inclusion: [true, false]
 end
